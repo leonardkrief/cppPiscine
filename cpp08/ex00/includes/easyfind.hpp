a87016702213aafb8e1a0e03dd52b19461ee77c6
Léonard Krief <53581910+leonardkrief@users.noreply.github.com>
2023-07-20 23:19:11 +0200

    nice and easy

--- cpp08/ex00/includes/easyfind.hpp
+++ cpp08/ex00/includes/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 12:25:49 by lkrief            #+#    #+#             */
-/*   Updated: 2023/07/20 19:22:19 by lkrief           ###   ########.fr       */
+/*   Updated: 2023/07/20 23:15:50 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,70 @@
 
 #include <iostream>
 #include <sstream>
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
 #include <algorithm>
 
 class ElementNotFound : public std::exception
 {
+    private:
+        int value_;
+        mutable std::string message_; // mutable because we will change it in a const function.
+
     public:
-        const char* what() const throw() { return "Element not found";};
+        explicit ElementNotFound(int value) : value_(value), message_("") {}
+
+        const char* what() const throw() 
+        {
+            std::ostringstream oss;
+            oss << value_ << " not found";
+            message_ = oss.str();
+            return message_.c_str();
+        }
 };
 
 template <typename T>
 typename T::difference_type easyfind ( T& A, int value )
 {
-    typename T::iterator sol = std::find(A.begin(), A.end(), value);
+    typename T::const_iterator sol = std::find(A.begin(), A.end(), value);
     if (sol == A.end())
     {
-        throw ElementNotFound();
+        throw ElementNotFound(value);
     }
-    typename T::difference_type index = std::distance(A.begin(), sol);
-    std::cout << value << " was found at position "
+    typename T::difference_type index = std::distance(A.cbegin(), sol);
+    std::cout << value << " found at position "
               << index << std::endl;
     return index;
 }
 
-template <template <int>>
-
-template <typename<T>>
-void printContainer( T<int> A)
+template
+  < template <typename Elem, typename Alloc=std::allocator<Elem> > class Container >
+std::ostream& operator<< ( std::ostream& o, const Container<int>& container )
 {
-    std::cout << "{ ";
-    for (std::vector<int>::iterator it = A.begin(); it != A.end(); it++)
+    typename Container<int>::const_iterator beg = container.begin();
+    o << "[";
+    while (beg != container.end()) { o << " " << *beg++; }
+    o << " ]";
+    return o;
+}
+
+template
+  < template <typename Elem, typename Alloc=std::allocator<Elem> > class Container >
+Container<int> randomContainerOfInts ( size_t n = 10, int min = 0, int max = 20 )
+{
+    Container<int> obj;
+
+    if (min > max)
     {
-        std::cout << *it << " ";
+        int tmp = min;
+        min = max;
+        max = tmp;
     }
-    std::cout << "}" << std::endl;
+    for (size_t i = 0; i < n; i++)
+    {
+        obj.push_back(min + (rand() % (max - min)));
+    }
+    return obj;
 }
 
 #endif
--- cpp08/ex00/sources/main.cpp
+++ cpp08/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 12:29:14 by lkrief            #+#    #+#             */
-/*   Updated: 2023/07/20 19:18:15 by lkrief           ###   ########.fr       */
+/*   Updated: 2023/07/20 23:18:45 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
 #include <vector>
 #include <list>
+#include <deque>
+#include <array>
 
 const unsigned int BANNER_SIZE = 55;
 
@@ -54,24 +58,73 @@ static void printTitle(std::string title, bool toUpper = true, int banner_size =
     std::cout << std::setfill(' ');
 }
 
-void vectorUnitTest()
-{
-    static int N = 0;
+const unsigned int NB_CONTAINERS = 3;
 
+void printUnitTestTitle( int c )
+{
+    static int N = 1;
+    const char* containers[NB_CONTAINERS] = {
+        "vector",
+        "list",
+        "deque",
+    };
     std::stringstream ss;
     ss << N;
-    std::string title = "vector unit test " + ss.str();
+    std::string title = "unit test " + ss.str() + ": " + containers[c];
     printTitle(title);
-    std::list<int> s = {20,3,2,1,2,3,1,2};
-    // printVector(s);
-    easyfind(s, 1);
     N++;
 }
 
-int main()
+void unitTest()
 {
-    vectorUnitTest();
-    vectorUnitTest();
-    vectorUnitTest();
-    vectorUnitTest();
+    try
+    {
+        int c = rand() % NB_CONTAINERS;
+        printUnitTestTitle(c);
+        switch (c)
+        {
+            case 0:
+            {
+                std::vector<int> myContainer = randomContainerOfInts<std::vector>();
+                std::cout << myContainer << std::endl;
+                easyfind(myContainer, rand() % 20);
+                break;
+            }
+            case 1:
+            {
+                std::list<int> myContainer = randomContainerOfInts<std::list>();
+                std::cout << myContainer << std::endl;
+                easyfind(myContainer, rand() % 20);
+                break;
+            }
+            case 2:
+            {
+                std::deque<int> myContainer = randomContainerOfInts<std::deque>();
+                std::cout << myContainer << std::endl;
+                easyfind(myContainer, rand() % 20);
+                break;
+            }
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+int main(int ac, char **av)
+{
+    srand(time(0));
+    if (ac == 2)
+    {
+        int n = atoi(av[1]);
+        for (int i = 0; i < n; i++)
+        {
+            unitTest();
+        }
+    }
+    else
+    {
+        unitTest();
+    }
 }
